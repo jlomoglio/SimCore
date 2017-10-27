f81e57c6c1210e58ce72d001fa27200959958765
ravi-tj <ravi.m@techjini.com>
2017-10-27 17:15:17 +0530

    issue fix of all 3 widgets of same size

--- src/widgets/Gear.vue
+++ src/widgets/Gear.vue
@@ -1,166 +1,168 @@
 
 <template>
-<div class="gear-container">
-  <div class="gear-BG">
+  <div class="gear-container">
+    <div class="gear-BG">
       <img id="gear-img" class="gear-img" src="/assets/img/module/gear_shift_movement_Sprite.png"/>
-  </div>
-  <div class="hot-spots">
-    <hint-box id="drive-hint" v-hint="driveShowHint"></hint-box>
-    <hint-box id="park-hint" v-hint="parkShowHint"></hint-box>
+    </div>
+    <div class="hot-spots">
+      <hint-box id="drive-hint" v-hint="driveShowHint"></hint-box>
+      <hint-box id="park-hint" v-hint="parkShowHint"></hint-box>
 
-    <div id="drive-hotspot" @click="clickable() ? drive() : ''"></div>
-    <div id="park-hotspot" @click="clickable() ? park() : ''"></div>
+      <div id="drive-hotspot" @click="clickable() ? drive() : ''"></div>
+      <div id="park-hotspot" @click="clickable() ? park() : ''"></div>
+    </div>
+
   </div>
-  
-</div>
 </template>
 <script>
-import { TweenMax, SteppedEase } from 'gsap'
-import HintBox from './HintBox'
-export default {
-    props: [
-        'currentTask',
-        'tasks',
-        'driveHint',
-        'parkHint'
-    ],
-    data() {
-        return {
-            row: 0,
-            driveShowHint: this.driveHint,
-            parkShowHint: this.parkHint
-        }
-    },
-    components: {
-        HintBox
-    },
-    watch: {
-        driveHint(newVal) {
-            return this.driveShowHint = newVal
+    import { TweenMax, SteppedEase } from 'gsap'
+    import HintBox from './HintBox'
+    export default {
+        props: [
+            'currentTask',
+            'tasks',
+            'driveHint',
+            'parkHint'
+        ],
+        data() {
+            return {
+                row: 0,
+                driveShowHint: this.driveHint,
+                parkShowHint: this.parkHint
+            }
         },
-        parkHint(newVal) {
-            return this.parkShowHint = newVal
-        }
-    },
-    methods: {
-        clickable() {
-            let key = -1
-            this.tasks.map((task, i) => {
-                if (this.currentTask === task) {
-                    key = i
+        components: {
+            HintBox
+        },
+        watch: {
+            driveHint(newVal) {
+                return this.driveShowHint = newVal
+            },
+            parkHint(newVal) {
+                return this.parkShowHint = newVal
+            }
+        },
+        methods: {
+            clickable() {
+                let key = -1
+                this.tasks.map((task, i) => {
+                    if (this.currentTask === task) {
+                        key = i
+                    }
+                })
+                if (key !== -1) {
+                    return true
                 }
-            })
-            if (key !== -1) {
-                return true
-            }
-            return false
-        },
-        drive() {
-            this.driveShowHint = false
-            this.$emit('correct')
-            this.putInDrive()
-        },
-        park() {
-            this.parkShowHint = false
-            this.$emit('correct')
-            this.row -= 1
-            this.putInPark()
-        },
-        putInDrive(ro) {
-            TweenMax.to(`#gear-img`, 1, {
-                marginLeft: `-464px`,
-                ease: SteppedEase.config(4),
-                onComplete: this.driveCallBack
-            })
-        },
-        driveCallBack() {
-            this.row += 1
-            if (this.row < 4) {
-                var sprite = document.getElementById('gear-img')
-                var yPosition = this.row * 116
-                sprite.style.marginLeft = '0px'
-                sprite.style.marginTop = `-${yPosition}px`
+                return false
+            },
+            drive() {
+                this.driveShowHint = false
+                this.$emit('correct')
                 this.putInDrive()
-            } else {
-                this.$emit('drive')
-            }
-        },
-        putInPark() {
-            TweenMax.to(`#gear-img`, 1, {
-                marginLeft: `0px`,
-                ease: SteppedEase.config(4),
-                onComplete: this.parkCallBack
-            })
-        },
-        parkCallBack() {
-            this.row -= 1
-            if (this.row >= 0) {
-                var sprite = document.getElementById('gear-img')
-                var yPosition = this.row * 116
-                sprite.style.marginLeft = '-464px'
-                sprite.style.marginTop = `-${yPosition}px`
+            },
+            park() {
+                this.parkShowHint = false
+                this.$emit('correct')
+                this.row -= 1
                 this.putInPark()
-            } else {
-                this.$emit('park')
+            },
+            putInDrive(ro) {
+                TweenMax.to(`#gear-img`, 1, {
+                    marginLeft: `-544px`,
+                    ease: SteppedEase.config(4),
+                    onComplete: this.driveCallBack
+                })
+            },
+            driveCallBack() {
+                this.row += 1
+                if (this.row < 4) {
+                    var sprite = document.getElementById('gear-img')
+                    var yPosition = this.row * 136
+                    sprite.style.marginLeft = '0px'
+                    sprite.style.marginTop = `-${yPosition}px`
+                    this.putInDrive()
+                } else {
+                    this.$emit('drive')
+                }
+            },
+            putInPark() {
+                TweenMax.to(`#gear-img`, 1, {
+                    marginLeft: `0px`,
+                    ease: SteppedEase.config(4),
+                    onComplete: this.parkCallBack
+                })
+            },
+            parkCallBack() {
+                this.row -= 1
+                if (this.row >= 0) {
+                    var sprite = document.getElementById('gear-img')
+                    var yPosition = this.row * 136
+                    sprite.style.marginLeft = '-544px'
+                    sprite.style.marginTop = `-${yPosition}px`
+                    this.putInPark()
+                } else {
+                    this.$emit('park')
+                }
             }
         }
     }
-}
 </script>
 <style scoped>
   .gear-BG {
     border-radius: 50%;
-    width: 120px;
-    height: 120px;
+    width: 140px;
+    height: 140px;
     overflow: hidden;
     box-sizing: border-box;
     border: 2px solid #a6a6a6;
     position: fixed;
+    margin-top: -14px;
+    margin-left: 7px;
   }
- #drive-hotspot {
-   position: absolute;
-   bottom: -82px;
-   left: 44px;
-   width: 7px;
-   height: 8px;
-   border-radius: 50%;
-   z-index: 5;
- }
- #park-hotspot {
-   position: absolute;
-   bottom: -70px;
-   left: 33px;
-   width: 6px;
-   height: 8px;
-   border-radius: 12px;
-   z-index: 5;
- }
+  #drive-hotspot {
+    position: absolute;
+    bottom: -80px;
+    left: 60px;
+    width: 7px;
+    height: 8px;
+    border-radius: 50%;
+    z-index: 5;
+  }
+  #park-hotspot {
+    position: absolute;
+    bottom: -68px;
+    left: 47px;
+    width: 6px;
+    height: 8px;
+    border-radius: 12px;
+    z-index: 5;
+  }
   #drive-hint{
-   position: absolute;
-   bottom: -83px;
-   left: 44px;
-   width: 7px;
-   height: 8px;
-   border-radius: 50%;
- }
- #park-hint {
-   position: absolute;
-   bottom: -70px;
-   left: 33px;
-   width: 6px;
-   height: 8px;
-   border-radius: 12px;
- }
- .gear-img {
+    position: absolute;
+    bottom: -80px;
+    left: 60px;
+    width: 7px;
+    height: 8px;
+    border-radius: 50%;
+  }
+  #park-hint {
+    position: absolute;
+    bottom: -68px;
+    left: 47px;
+    width: 6px;
+    height: 8px;
+    border-radius: 12px;
+  }
+  .gear-img {
     width: 500%;
     height: 400%;
     margin-left: 0px;
     margin-top: 0px;
     position: relative;
- }
- .hot-spots {
+  }
+  .hot-spots {
     position: relative;
     z-index: 2;
     height: 0;
- }
+  }
 </style>
--- src/widgets/ignition-switch/IgnitionSwitch.vue
+++ src/widgets/ignition-switch/IgnitionSwitch.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="ignition-switch"
-        :style="{ left: left, top: top, right: right, bottom: bottom }"
+         :style="{ left: left, top: top, right: right, bottom: bottom }"
     >
         <div id="ignition-switch-plate" :class="ignitionOn ? 'on' : 'off'">
-            <div id="ignition-key"></div>
+            <div id="ignition-key" :class="ignitionOn ? 'on' : 'off'"></div>
         </div>
 
         <hint-box id="lock-hint" v-hint="lockShowHint"></hint-box>
@@ -18,301 +18,304 @@
 </template>
 
 <script>
-import { TweenMax, Draggable } from 'gsap'
-import HintBox from '../HintBox'
+    import { TweenMax, Draggable } from 'gsap'
+    import HintBox from '../HintBox'
 
-export default {
-    name: 'IgnitionSwitch',
-    props: [
-        'left',
-        'top',
-        'right',
-        'bottom',
-        'lockHint',
-        'accHint',
-        'onHint',
-        'currentTask',
-        'tasks',
-        'currentAction'
-    ],
-    components: {
-        HintBox
-    },
-    data() {
-        return {
-            ignitionOn: false,
-            lockShowHint: this.lockHint,
-            accShowHint: this.accHint,
-            onShowHint: this.onHint
-        }
-    },
-    mounted() {
-        // Reassign this
-        let vm = this
-        // Setup Draggable
-        Draggable.create('#ignition-key', {
-            type: 'rotation',
-            throwProps: true,
-            zIndexBoost: false,
-            bounds: {
-                minRotation: 55,
-                maxRotation: 160
-            },
-            onDrag: function() {
+    export default {
+        name: 'IgnitionSwitch',
+        props: [
+            'left',
+            'top',
+            'right',
+            'bottom',
+            'lockHint',
+            'accHint',
+            'onHint',
+            'currentTask',
+            'tasks',
+            'currentAction'
+        ],
+        components: {
+            HintBox
+        },
+        data() {
+            return {
+                ignitionOn: false,
+                lockShowHint: this.lockHint,
+                accShowHint: this.accHint,
+                onShowHint: this.onHint
+            }
+        },
+        mounted() {
+            // Reassign this
+            let vm = this
+            // Setup Draggable
+            Draggable.create('#ignition-key', {
+                type: 'rotation',
+                throwProps: true,
+                zIndexBoost: false,
+                bounds: {
+                    minRotation: 55,
+                    maxRotation: 160
+                },
+                onDrag: function() {
 
-            },
-            onRelease: function() {
-                // LOCK Mode
-                if (this.rotation >= 55 && this.rotation < 70) {
-                    if (this.currentAction === 'lock') {
-                        TweenMax.set('#ignition-key', {
-                            rotation: 55
-                        })
-                        vm.lockShowHint = false
-                        vm.ignitionOn = false
-                        vm.$emit('lock')
+                },
+                onRelease: function() {
+                    // LOCK Mode
+                    if (this.rotation >= 55 && this.rotation < 70) {
+                        if (this.currentAction === 'lock') {
+                            TweenMax.set('#ignition-key', {
+                                rotation: 55
+                            })
+                            vm.lockShowHint = false
+                            vm.ignitionOn = false
+                            vm.$emit('lock')
+                        }
                     }
-                }
-                // ACC Mode
-                else if (this.rotation > 80 && this.rotation < 112) {
-                    TweenMax.set('#ignition-key', {
-                        rotation: 95
-                    })
-                    vm.accShowHint = false
-                    vm.ignitionOn = false
-                    vm.$emit('acc')
-                }
-                // ON Mode
-                else if (this.rotation > 115 && this.rotation < 125) {
-                    if (this.currentAction === 'on') {
+                    // ACC Mode
+                    else if (this.rotation > 80 && this.rotation < 112) {
                         TweenMax.set('#ignition-key', {
+                            rotation: 95
+                        })
+                        vm.accShowHint = false
+                        vm.ignitionOn = false
+                        vm.$emit('acc')
+                    }
+                    // ON Mode
+                    else if (this.rotation > 115 && this.rotation < 125) {
+                        if (this.currentAction === 'on') {
+                            TweenMax.set('#ignition-key', {
+                                rotation: 122
+                            })
+                            vm.onShowHint = false
+                            vm.ignitionOn = true
+                            vm.$emit('on')
+                        }
+                    }
+                    // START Mode
+                    else if (this.rotation > 135 && this.rotation <= 160) {
+                        TweenMax.to('#ignition-key', 1.0, {
                             rotation: 122
                         })
                         vm.onShowHint = false
                         vm.ignitionOn = true
-                        vm.$emit('on')
+                        vm.$emit('start')
                     }
                 }
-                // START Mode
-                else if (this.rotation > 135 && this.rotation <= 160) {
+            })
+        },
+        watch: {
+            lockHint(newVal) {
+                return this.lockShowHint = newVal
+            },
+            accHint(newVal) {
+                return this.accShowHint = newVal
+            },
+            onHint(newVal) {
+                return this.onShowHint = newVal
+            }
+        },
+        methods: {
+            clickable() {
+                let key = -1
+                this.tasks.map((task, i) => {
+                    if (this.currentTask === task) {
+                        key = i
+                    }
+                })
+                if (key !== -1) {
+                    return true
+                }
+                return false
+            },
+            // Rotates Key based on mode that is clicked
+            setMode(mode) {
+                // Reassign this
+                let vm = this
+
+                // LOCK mode
+                if (mode === 'lock' && this.currentAction === 'lock') {
+                    TweenMax.to('#ignition-key', 1.0, {
+                        rotation: 55
+                    })
+                    vm.lockShowHint = false
+                    this.ignitionOn = false
+                    this.$emit('lock')
+                }
+                // ACC Mode
+                else if (mode === 'acc' && this.currentAction === 'acc') {
+                    TweenMax.to('#ignition-key', 1.0, {
+                        rotation: 95
+                    })
+                    vm.accShowHint = false
+                    this.ignitionOn = false
+                    this.$emit('acc')
+                }
+                // ON Mode
+                else if (mode === 'on' && this.currentAction === 'on') {
                     TweenMax.to('#ignition-key', 1.0, {
                         rotation: 122
                     })
-                    vm.onShowHint = false
-                    vm.ignitionOn = true
-                    vm.$emit('start')
+                    this.onShowHint = false
+                    this.ignitionOn = true
+                    this.$emit('on')
                 }
-            }
-        })
-    },
-    watch: {
-        lockHint(newVal) {
-            return this.lockShowHint = newVal
-        },
-        accHint(newVal) {
-            return this.accShowHint = newVal
-        },
-        onHint(newVal) {
-            return this.onShowHint = newVal
-        }
-    },
-    methods: {
-        clickable() {
-            let key = -1
-            this.tasks.map((task, i) => {
-                if (this.currentTask === task) {
-                    key = i
+                // START mode
+                else if (mode === 'start' && this.currentAction === 'start') {
+                    TweenMax.to('#ignition-key', 1.0, {
+                        rotation: 160,
+                        onComplete: function() {
+                            vm.setToOn()
+                        }
+                    })
+                    this.onShowHint = false
+                    this.ignitionOn = true
+                    this.$emit('start')
                 }
-            })
-            if (key !== -1) {
-                return true
-            }
-            return false
-        },
-        // Rotates Key based on mode that is clicked
-        setMode(mode) {
-            // Reassign this
-            let vm = this
+            },
 
-            // LOCK mode
-            if (mode === 'lock' && this.currentAction === 'lock') {
-                TweenMax.to('#ignition-key', 1.0, {
-                    rotation: 55
-                })
-                vm.lockShowHint = false
-                this.ignitionOn = false
-                this.$emit('lock')
-            }
-            // ACC Mode
-            else if (mode === 'acc' && this.currentAction === 'acc') {
-                TweenMax.to('#ignition-key', 1.0, {
-                    rotation: 95
-                })
-                vm.accShowHint = false
-                this.ignitionOn = false
-                this.$emit('acc')
-            }
-            // ON Mode
-            else if (mode === 'on' && this.currentAction === 'on') {
+            // Rotates key back to ON position
+            setToOn() {
                 TweenMax.to('#ignition-key', 1.0, {
                     rotation: 122
                 })
-                this.onShowHint = false
                 this.ignitionOn = true
                 this.$emit('on')
             }
-            // START mode
-            else if (mode === 'start' && this.currentAction === 'start') {
-                TweenMax.to('#ignition-key', 1.0, {
-                    rotation: 160,
-                    onComplete: function() {
-                        vm.setToOn()
-                    }
-                })
-                this.onShowHint = false
-                this.ignitionOn = true
-                this.$emit('start')
-            }
-        },
-
-        // Rotates key back to ON position
-        setToOn() {
-            TweenMax.to('#ignition-key', 1.0, {
-                rotation: 122
-            })
-            this.ignitionOn = true
-            this.$emit('on')
         }
     }
-}
 </script>
 
 <style scoped>
-#ignition-switch {
-    position: absolute;
-    width: 150px;
-    height: 150px;
-    margin-left: 1%;
-}
+    #ignition-switch {
+        position: absolute;
+        width: 150px;
+        height: 150px;
+        margin-left: 1%;
+    }
 
-#lock-hotspot {
-    position: absolute;
-    top: 33px;
-    left: 40px;
-    width: 25px;
-    height: 25px;
-    border-radius: 12px;
-    z-index: 15px;
-}
+    #lock-hotspot {
+        position: absolute;
+        top: 36px;
+        left: 43px;
+        width: 25px;
+        height: 25px;
+        border-radius: 12px;
+        z-index: 15px;
+    }
 
-#lock-hint {
-    position: absolute;
-    top: 33px;
-    left: 40px;
-    width: 25px;
-    height: 25px;
-    border-radius: 12px;
-    z-index: 15px;
-}
+    #lock-hint {
+        position: absolute;
+        top: 36px;
+        left: 43px;
+        width: 25px;
+        height: 25px;
+        border-radius: 12px;
+        z-index: 15px;
+    }
 
-#acc-hotspot {
-    position: absolute;
-    top: 27px;
-    left: 67px;
-    width: 20px;
-    height: 20px;
-    border-radius: 10px;
-    z-index: 7px;
-}
+    #acc-hotspot {
+        position: absolute;
+        top: 31px;
+        left: 73px;
+        width: 20px;
+        height: 20px;
+        border-radius: 10px;
+        z-index: 7px;
+    }
 
-#acc-hint {
-    position: absolute;
-    top: 27px;
-    left: 67px;
-    width: 20px;
-    height: 20px;
-    border-radius: 10px;
-    z-index: 7px;
-}
+    #acc-hint {
+        position: absolute;
+        top: 31px;
+        left: 73px;
+        width: 20px;
+        height: 20px;
+        border-radius: 10px;
+        z-index: 7px;
+    }
 
-#on-hotspot {
-    position: absolute;
-    top: 34px;
-    left: 82px;
-    width: 18px;
-    height: 18px;
-    border-radius: 9px;
-    z-index: 7px;
-}
+    #on-hotspot {
+        position: absolute;
+        top: 35px;
+        left: 94px;
+        width: 18px;
+        height: 18px;
+        border-radius: 9px;
+        z-index: 7px;
+    }
 
-#on-hint {
-    position: absolute;
-    top: 34px;
-    left: 82px;
-    width: 18px;
-    height: 18px;
-    border-radius: 9px;
-    z-index: 7px;
-}
+    #on-hint {
+        position: absolute;
+        top: 35px;
+        left: 94px;
+        width: 18px;
+        height: 18px;
+        border-radius: 9px;
+        z-index: 7px;
+    }
 
-#start-hotspot {
-    position: absolute;
-    top: 45px;
-    left: 90px;
-    width: 30px;
-    height: 30px;
-    border-radius: 15px;
-    z-index: 10px !important;
-}
+    #start-hotspot {
+        position: absolute;
+        top: 50px;
+        left: 106px;
+        width: 30px;
+        height: 30px;
+        border-radius: 15px;
+        z-index: 10px !important;
+    }
 
-#start-hint {
-    position: absolute;
-    top: 45px;
-    left: 90px;
-    width: 30px;
-    height: 30px;
-    border-radius: 15px;
-    z-index: 10px !important;
-}
+    #start-hint {
+        position: absolute;
+        top: 50px;
+        left: 106px;
+        width: 30px;
+        height: 30px;
+        border-radius: 15px;
+        z-index: 10px !important;
+    }
 
-#on-start-hint {
-    position: absolute;
-    top: 38px;
-    left: 78px;
-    width: 55px;
-    height: 20px;
-    border-top-left-radius: 20px;
-    border-top-right-radius: 20px;
-    border-bottom-right-radius: 20px;
-    border-bottom-left-radius: 20px;
-    transform: rotate(50deg);
-    z-index: 10px !important;
-}
+    #on-start-hint {
+        position: absolute;
+        top: 38px;
+        left: 78px;
+        width: 55px;
+        height: 20px;
+        border-top-left-radius: 20px;
+        border-top-right-radius: 20px;
+        border-bottom-right-radius: 20px;
+        border-bottom-left-radius: 20px;
+        transform: rotate(50deg);
+        z-index: 10px !important;
+    }
 
-#ignition-switch-plate {
-    width: 125px;
-    height: 127px;
-    margin-top: 12px;
-    margin-left: 8%;
-}
+    #ignition-switch-plate {
+        width: 140px;
+        height: 140px;
+        margin-top: 12px;
+        margin-left: 8%;
+    }
 
-#ignition-switch-plate.on {
-    background: url('/assets/img/module/ignition_key_switch_on.png');
-    background-size: 100%;
-}
+    #ignition-switch-plate.on {
+        background: url('./ignition_key_switch_on.png');
+        background-size: 100%;
+    }
 
-#ignition-switch-plate.off {
-    background: url('/assets/img/module/ignition_key_switch_off.png');
-    background-size: 100%;
-}
+    #ignition-switch-plate.off {
+        background: url('./ignition_key_switch_off.png');
+        background-size: 100%;
+    }
 
-#ignition-key {
-    margin: 0 auto;
-    width: 126px;
-    height: 126px;
-    background: url('/assets/img/module/key_use.png') no-repeat;
-    background-size: 105%;
-    background-position: -320% 280%;
-    transform: rotate(0deg); /* lock: 55deg, acc: 100, on: 120, start: 155 */
-}
+    #ignition-key {
+        margin: 0 auto;
+        width: 130px;
+        height: 131px;
+        background: url('./key_use.png') no-repeat;
+        background-size: 106%;
+        background-position: -320% 280%;
+        transform: rotate(0deg); /* lock: 55deg, acc: 100, on: 120, start: 155 */
+    }
+    #ignition-key.on {
+        height: 129px;
+    }
 </style>
